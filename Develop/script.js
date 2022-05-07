edc482e85b12bc64072094d41be773435807e400
Avyrie Fellows <avyriefellows@gmail.com>
2022-05-07 08:14:46 -0600

    Cleaned up console logs, psuedocode, and comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,3 @@
-// Assignment code here
 
 var passwordlength;
 var symbolConfirm;
@@ -17,8 +16,6 @@ lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "
 
 
 
-// console.log(symbol)
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 
@@ -28,18 +25,13 @@ function generatePassword() {
   while(passwordlength < 8 || passwordlength > 128 || isNaN(passwordlength)) {
   passwordlength = Number(prompt("Choose password length: 8 - 128 characters"));
   }
-  console.log(passwordlength);
-  
+
   while(!lowerConfirm && !upperConfirm && !symbolConfirm && !numConfirm) {
     lowerConfirm = confirm("Would you like Lowercase letters?");
-    console.log(lowerConfirm);
     upperConfirm = confirm("Would you like Uppercase letters?");
-    console.log(upperConfirm);
     symbolConfirm = confirm("Would you like Symbols?");
-    console.log(symbolConfirm);
     numConfirm = confirm("Would you like Numbers?");
-    console.log(numConfirm);
-  }
+      }
   
   charChoice = "";
   if (lowerConfirm) {charChoice = charChoice.concat(lower);}
@@ -58,7 +50,7 @@ function generatePassword() {
 }
 
 
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -67,19 +59,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-//PsuedoCode
-
-//Prompt user to choose between 8 and 128 characters
-  //if they input number outside of range or leave empty; say "Invalid answer" and loop prompt till correct input is given
-      //use a loop in this area
-
-//Confirm whether user would like symbols, Upper, Lower, or numbers within their password
-  // use if statements to determine whether the user's choices are used or not
-
-//Created Password is then written to the page or as an alert for the user to view
